b3827cd8686ba2386337989e49d2248d2f5c008d
Sanke <86974454+San245o@users.noreply.github.com>
2023-09-01 22:23:37 +0530

    updated ketones and other functional groups

--- iupac.py
+++ iupac.py
@@ -11,10 +11,12 @@ class Compound:
             print("Methane")
         else:
             root_word = ["Meth", "Eth", "Prop", "But", "Pent", "Hex", "Hept", "Oct", "Non", "Dec"]
-            functionals = ['Cl','Br','F','I','-(OH)','-(CO)-','-(CHO)',]
+            functionals = ['-(OH)','-(CO)-','-(CHO)']
+            halogens = ['Cl','Br','F','I']
             secondary_prefix = ''
             cyc = ''
             FuncTrue = False
+            halTrue = False
 
             for i in self.compound:
                 if i == "H":
@@ -27,13 +29,20 @@ class Compound:
                     func_place = self.compound.find(i)
                     self.functional_group = i
                     FuncTrue = True
+
+            for i in halogens:
+                if i in self.compound:
+                    hal_place = self.compound.find(i)
+                    self.halogen_grp = i
+                    halTrue = True
             
             c_atoms  = int(self.compound[carb_place+1:hyd_place])
             if FuncTrue: 
-                hyd_atoms = int(self.compound[hyd_place+1:func_place])
+                hyd_atoms = int(self.compound[hyd_place+1:func_place]) + 1
+            elif halTrue:
+                hyd_atoms = int(self.compound[hyd_place+1:hal_place])
             else:
                 hyd_atoms = int(self.compound[hyd_place+1:])
-
 
             if hyd_atoms == 2*c_atoms + 2:
                 self.secondary_prefix += 'ane'
@@ -46,25 +55,22 @@ class Compound:
             elif hyd_atoms == c_atoms:
                 cyc += 'Cyclo-'
                 self.secondary_prefix += 'ane' 
-            hal_ = ['Cl','Br','F','I']
+
             alc_ = ['-(OH)']
             ald_ = ['-(CHO)']
             ket_ = ['-(CO)-']
+
             if FuncTrue:
-                if self.functional_group in hal_:
-                    self.Halogen(root_word[c_atoms - 1])
                 if self.functional_group in alc_:
                     self.Alcohols(root_word[c_atoms - 1])
                 if self.functional_group in ald_:
                     self.Aldehydes(root_word[c_atoms])
                 if self.functional_group in ket_:
                     self.Ketones()
-
-                    
+            elif halTrue:
+                self.Halogen(root_word[c_atoms - 1])
             else:
                 self.Hydrocarbons(root_word[c_atoms - 1])
-
-
 
     def Hydrocarbons(self,root_word):
         self.root_word = root_word
@@ -75,24 +81,54 @@ class Compound:
         self.root_word = root_word
         print("Halogen detected\n")
         hal = {'Cl': 'Chloride', 'Br': 'Bromide', 'I': 'Iodide', 'F': 'Fluoride'}
-        hal_prefix = hal.get(self.functional_group, "Unknown")
+        hal_prefix = hal.get(self.halogen_grp, "Unknown")
         print(f'The IUPAC name of your given compound is: {self.cyc + self.root_word + self.secondary_prefix + hal_prefix}')
 
     def Alcohols(self,root_word):
         self.root_word = root_word
-        print("alcohols")
         print(f'The IUPAC name of your given compound is: {self.cyc + self.root_word + self.secondary_prefix + "ol"}')
 
     
     def Aldehydes(self,root_word):
-        print("aldehydes")
         self.root_word = root_word
         print(f'The IUPAC name of your given compound is: {self.cyc + self.root_word + self.secondary_prefix + "-al"}')
 
 
     def Ketones(self):
-        print("ketones")
-        pass 
-    
-c = input("enter a compound: ")
-ele = Compound(c)
+        print("KETONES")
+        lst = self.compound.split("-(CO)-")
+
+
+        def findpac(compound):
+
+            c_place = compound.find("C")
+            h_place = compound.find("H")
+            c_atoms = int(compound[c_place + 1:h_place])
+            h_atoms = int(compound[h_place + 1:])
+
+            root_word = ["Meth", "Eth", "Prop", "But", "Pent", "Hex", "Hept", "Oct", "Non", "Dec"]
+            secondary_prefix = ''
+            cyc = ''
+            if h_atoms == 2*c_atoms + 2:
+                secondary_prefix += 'ane'
+            elif h_atoms == 2*c_atoms:
+                secondary_prefix += 'ene'
+            elif h_atoms == 2*c_atoms - 2:
+                secondary_prefix += 'yne'
+            elif h_atoms == 2*c_atoms + 1:
+                secondary_prefix += 'yl'
+            elif h_atoms == c_atoms:
+                cyc += 'Cyclo-'
+                secondary_prefix += 'ane'
+
+            return cyc + root_word[c_atoms-1] + secondary_prefix
+        R1 = findpac(lst[0])
+        R2 = findpac(lst[1])
+
+        if R1 == R2:
+            print((f"The IUPAC name of your given compound is: {'di'+ R1+'Ketone'}"))
+        else:
+            print(f"The IUPAC name of your given compound is: {R1 + R2 + 'Ketone'}")
+        
+#c = input("enter a compound: ").upper()
+ele = Compound("C2H5-(CO)-C2H5")
